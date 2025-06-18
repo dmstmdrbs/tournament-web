156b81e2ee888c533eac4c5559719317c5df3087
dmstmdrbs <dmstmdrbs98@gmail.com>
2025-06-18 18:49:41 +0900

    feat: static build & github actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+const repoName = "tournament-web";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  // 아래 두 줄은 서브 디렉토리 배포 시 필요 (예: https://username.github.io/레포이름)
+  basePath: isGithubPages ? `/${repoName}` : "",
+  assetPrefix: isGithubPages ? `/${repoName}/` : "",
 };
 
 export default nextConfig;
