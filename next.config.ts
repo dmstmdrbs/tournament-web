302bacc14cff0803cedf28e99805bef8506dd9d9
dmstmdrbs <dmstmdrbs98@gmail.com>
2025-06-19 10:43:52 +0900

    chore: output 수정

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const isGithubPages = process.env.GITHUB_PAGES === "true";
 const repoName = "tournament-web";
 
 const nextConfig: NextConfig = {
+  output: "export",
   // 아래 두 줄은 서브 디렉토리 배포 시 필요 (예: https://username.github.io/레포이름)
   basePath: isGithubPages ? `/${repoName}` : "",
   assetPrefix: isGithubPages ? `/${repoName}/` : "",
